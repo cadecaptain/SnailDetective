38132e49c356d8a94f855815dfc3e45d41a95529
Cade <phillipscb@hendrix.edu>
2021-02-14 15:01:01 -0600

    Snail Moving and Camera following

---
+++ SnailDetectiveGame/Assets/Sources/Sources.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//https://opengameart.org/content/snail-1
+
+public class Sources : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
