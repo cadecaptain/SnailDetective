0f66f682cf3d2aeb6ef8515384efe4081c1e2d7c
Cade <phillipscb@hendrix.edu>
2021-02-16 17:43:29 -0600

    a few building changes and new snails

---
+++ SnailDetectiveGame/Assets/Scripts/Sources1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//https://pixy.org/114950/
+//https://opengameart.org/content/snail-1
+//https://emily2.itch.io/modern-city-top-down-tileset-16x16
+//https://commons.wikimedia.org/wiki/File:Skull_%26_Crossbones_BW.png
+
+public class Sources : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
